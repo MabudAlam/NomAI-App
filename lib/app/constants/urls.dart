cf77a23aaddf9cd18223711b727cf6a38cf2c6f9
Pavel401 <pavelalam401@gmail.com>
2026-04-19 15:40:35 +0530

    refactor: Update API URLs for nutrition analysis and messaging

--- lib/app/constants/urls.dart
+++ lib/app/constants/urls.dart
@@ -1,7 +1,15 @@
-class BaseUrl {
-  static const String baseUrl = 'http://10.0.2.2:8080/'; // Instead of 127.0.0.1
+class ApiUrl {
+  static const String baseUrl =
+      "https://46d0-2a09-bac5-3e0d-1a46-00-29e-85.ngrok-free.app";
 }
 
 class ApiPath {
-  static const String getNutritionData = "/nutrition/get";
+  static const String nutrtionByImage = "/api/v1/nutrition/analyze";
+
+  static const String sendMessage = "/api/v1/chat/messages";
+
+  static String getMessages(String userId, int offset, int limit) =>
+      "/api/v1/users?user_id=$userId&offset=$offset&limit=$limit";
+
+  static const String updateLogStatus = "/api/v1/users/log-status";
 }
