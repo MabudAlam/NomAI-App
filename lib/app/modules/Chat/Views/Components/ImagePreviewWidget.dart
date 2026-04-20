7a8bcdf39f078aa52b4c72a2a68fdf9883e56998
Pavel401 <pavelalam401@gmail.com>
2026-04-20 09:16:59 +0530

    fix: Update ImagePreviewWidget layout for better responsiveness and consistency

--- lib/app/modules/Chat/Views/Components/ImagePreviewWidget.dart
+++ lib/app/modules/Chat/Views/Components/ImagePreviewWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:NomAi/app/constants/colors.dart';
 import 'package:NomAi/app/modules/Chat/Controllers/ChatController.dart';
 
@@ -12,10 +13,10 @@ class ImagePreviewWidget extends StatelessWidget {
     return Stack(
       children: [
         Container(
-          width: 64,
-          height: 64,
+          width: 8.h,
+          height: 8.h,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.circular(1.5.h),
             image: DecorationImage(
               image: FileImage(controller.selectedImage.value!),
               fit: BoxFit.cover,
@@ -23,19 +24,19 @@ class ImagePreviewWidget extends StatelessWidget {
           ),
         ),
         Positioned(
-          top: -6,
-          right: -6,
+          top: -0.8.h,
+          right: -0.8.h,
           child: GestureDetector(
             onTap: () => controller.clearSelectedImage(),
             child: Container(
-              padding: const EdgeInsets.all(4),
+              padding: EdgeInsets.all(0.5.h),
               decoration: const BoxDecoration(
                 color: NomAIColors.darkPrimary,
                 shape: BoxShape.circle,
               ),
-              child: const Icon(
+              child: Icon(
                 Icons.close,
-                size: 14,
+                size: 12.sp,
                 color: NomAIColors.whiteText,
               ),
             ),
