337363ca241fe0216e316441c99c3c5dfe5b4130
Pavel401 <pavelalam401@gmail.com>
2026-04-20 09:12:42 +0530

    fix: Update ChatHeader to use AppConstants.appName and improve layout

--- lib/app/modules/Chat/Views/Components/ChatHeader.dart
+++ lib/app/modules/Chat/Views/Components/ChatHeader.dart
@@ -1,3 +1,4 @@
+import 'package:NomAi/app/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:NomAi/app/constants/colors.dart';
@@ -11,24 +12,17 @@ class ChatHeader extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 1.5.h),
       child: Row(
         children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: NomAIColors.whiteText.withValues(alpha: 0.2),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Icon(
-              Icons.chat_bubble_rounded,
-              color: NomAIColors.whiteText,
-              size: 24,
-            ),
+          Image.asset(
+            'assets/png/logo.png',
+            width: 5.h,
+            height: 5.h,
           ),
-          SizedBox(width: 3.w),
+          SizedBox(width: 2.w),
           Text(
-            'NomAI',
+            AppConstants.appName,
             style: TextStyle(
               color: NomAIColors.whiteText,
-              fontSize: 22,
+              fontSize: 18.sp,
               fontWeight: FontWeight.bold,
             ),
           ),
